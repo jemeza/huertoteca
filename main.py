fb057021264629981c4f392fbfbc6379be3bcf1b
Jesus Meza <jemeza@stanford.edu>
2023-03-13 19:18:07 +0100

    bug fix

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def home():
     if request.method == "POST":
         print(request.form["name"])
         print(request.form["email"])
-        tiempo_actual = datetime.datetime.now().timestamp()
-        # tiempo_actual = tiempo_actual + datetime.timedelta(seconds=1)
-        CONTROL_CENTER.schedule.enterabs(tiempo_actual, 2, CONTROL_CENTER.evento_poner_show, kwargs = {"scheduled_time":None, "duracion":.25 * 60})
+        tiempo_actual = datetime.datetime.now()
+        tiempo_actual = tiempo_actual + datetime.timedelta(seconds=.5)
+        CONTROL_CENTER.schedule.enterabs(tiempo_actual.timestamp(), 2, CONTROL_CENTER.evento_poner_show, kwargs = {"scheduled_time":None, "duracion":.25 * 60})
         return render_template('home.html')
     return render_template('home.html')
 
